fe521f275e18ee49f3b14d26af1aa4c2b0eb2aa8
William Esteban <william.esteban@campusucc.edu.co>
2023-01-26 00:52:11 -0500

    first commit

--- ciclos/src/Switch.java
+++ ciclos/src/Switch.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Switch {
+public class Switch {
+
+    public static void main(String[] args) {
+        String clima = "sunnylkj";
+
+
+        switch (clima){
+
+            case "sunny":
+
+                System.out.println("el tiempo es soleado");
+                break;
+
+            case "cloudy":
+
+                System.out.println("el tiempo es nublado");
+                break;
+            case "rainy":
+                System.out.println("esta lloviendo");
+                break;
+
+            default:
+                System.out.println("default");
+                break;
+
+
+
+        }
+    }
 }
